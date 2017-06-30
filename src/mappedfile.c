9b84c99eddf4c533ee8b7dd25c72d6758f8849fd
Partha Susarla <mail@spartha.org>
2017-06-30 15:46:53 +1000

    Placeholder for mappedfile api.

---
+++ src/mappedfile.c
@@ -0,0 +1,30 @@
+/*
+ * twoskip
+ *
+ *
+ * twoskip is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ *
+ * Copyright (c) 2017 Partha Susarla <mail@spartha.org>
+ *
+ */
+
+#include "mappedfile.h"
+
+int mappedfile_open(const char *name, uint32_t flags, struct mappedfile **mfp)
+{
+        struct mappedfile *mf;
+        int ret = 0;
+
+
+        *mfp = mf;
+
+        return ret;
+}
+
+int mappedfile_close(struct mappedfile **mfp)
+{
+        int ret = 0;
+
+        return ret;
+}
---
+++ src/mappedfile.h
@@ -0,0 +1,39 @@
+/*
+ * twoskip
+ *
+ *
+ * twoskip is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ *
+ * Copyright (c) 2017 Partha Susarla <mail@spartha.org>
+ *
+ */
+
+#ifndef _MAPPEDFILE_H_
+#define _MAPPEDFILE_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "macros.h"
+
+CPP_GUARD_START
+
+struct mappedfile {
+        char *fname;
+        int fd;
+        size_t size;
+        size_t offset;
+        uint32_t flags;
+};
+
+enum {
+        MAPPEDFILE_CREATE = (1 << 0),
+        MAPPEDFILE_RD     = (1 << 1),
+        MAPPEDFILE_WR     = (1 << 2),
+        MAPPEDFILE_RW     = (1 << 3),
+};
+
+CPP_GUARD_END
+
+#endif  /* _MAPPEDFILE_H_ */
