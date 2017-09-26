a52daedbe95da6fa2832e97a9ab9700c4971115a
Partha Susarla <mail@spartha.org>
2017-09-26 16:29:25 +1000

    Comments reflect project name

--- src/util.h
+++ src/util.h
@@ -1,8 +1,8 @@
 /*
- * twoskip
+ * skiplistdb
  *
  *
- * twoskip is free software; you can redistribute it and/or modify
+ * skiplistdb is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
  *
  * Copyright (c) 2017 Partha Susarla <mail@spartha.org>
