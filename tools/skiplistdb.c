95f7059951d94e0d100c52ee521092bff824b063
Partha Susarla <mail@spartha.org>
2017-10-05 15:56:43 +1100

    Update the tool to test the api

--- tools/skiplistdb.c
+++ tools/skiplistdb.c
@@ -7,8 +7,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "skiplistdb.h"
+
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
+        struct skiplistdb *db;
+        struct txn *tid;
+
+        db = skiplistdb_new(TWO_SKIP);
+        if (!db) {
+                fprintf(stderr, "Failed creating twoskip db object");
+                exit(EXIT_FAILURE);
+        }
+
+        if (skiplistdb_open(db, "foobar", 5, &tid) != SDB_OK) {
+                fprintf(stderr, "opening of db not successful!\n");
+        }
+
+        if (skiplistdb_close(db) != SDB_OK) {
+                fprintf(stderr, "closing of db not successful!\n");
+        }
+
+        skiplistdb_free(db);
         exit(EXIT_SUCCESS);
 }
 
